850527b1a58864b182f3b6ed35f02ce165f66701
Josu Martinez <josu.martinez@gmail.com>
2024-12-26 00:59:13 +0100

    fix: add conditional creation of ECR

--- deployment/ecr/ecr.tf
+++ deployment/ecr/ecr.tf
@@ -1,5 +1,11 @@
+# 
+data "aws_ecr_repository" "existing_repo" {
+  name = "dummy-app-ecr-repository"
+}
+
 # Create an ECR repository for the dummy-app
 resource "aws_ecr_repository" "dummy-app-ecr-repository" {
+  count = length(data.aws_ecr_repository.existing_repo.id) == 0 ? 1 : 0
   name = "dummy-app-ecr-repository"
 }
 
