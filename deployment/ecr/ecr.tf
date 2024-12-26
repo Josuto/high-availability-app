696d066d6fc9b7409b4e83702e0a571c3c4bacb8
Josu Martinez <josu.martinez@gmail.com>
2024-12-26 12:13:16 +0100

    fix: remove ECR anti-re-creation code

--- deployment/ecr/ecr.tf
+++ deployment/ecr/ecr.tf
@@ -1,13 +1,6 @@
 # Create an ECR repository for the dummy-app
 resource "aws_ecr_repository" "dummy-app-ecr-repository" {
   name = "dummy-app-ecr-repository"
-
-  lifecycle {
-    ignore_changes = [
-      # Add any attributes you don't want to trigger recreation
-      name,
-    ]
-  }
 }
 
 # Output the ECR repository URL to consume in the GitHub Actions pipeline
