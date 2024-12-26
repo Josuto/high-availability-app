eb4d0d46e8c0946c5f157e8f953a37fa549a53ad
Josu Martinez <josu.martinez@gmail.com>
2024-12-26 11:58:48 +0100

    fix: add workaround to prevent ECR re-creation

--- deployment/ecr/ecr.tf
+++ deployment/ecr/ecr.tf
@@ -1,15 +1,16 @@
-# Required to conditionally create an ECR repository based on its existence
-data "aws_ecr_repository" "existing_repo" {
-  name = "dummy-app-ecr-repository"
-}
-
 # Create an ECR repository for the dummy-app
 resource "aws_ecr_repository" "dummy-app-ecr-repository" {
-  count = length(data.aws_ecr_repository.existing_repo.id) == 0 ? 1 : 0
   name = "dummy-app-ecr-repository"
+
+  lifecycle {
+    ignore_changes = [
+      # Add any attributes you don't want to trigger recreation
+      name,
+    ]
+  }
 }
 
 # Output the ECR repository URL to consume in the GitHub Actions pipeline
 output "dummy-app-ecr-repository-url" {
-  value = length(aws_ecr_repository.dummy-app-ecr-repository) > 0 ? aws_ecr_repository.dummy-app-ecr-repository[0].repository_url : data.aws_ecr_repository.existing_repo.repository_url
+  value = aws_ecr_repository.dummy-app-ecr-repository.repository_url
 }
