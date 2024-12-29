fc9652190c4399aac8de26343414d8590584f111
Josu Martinez <josu.martinez@gmail.com>
2024-12-29 12:19:52 +0100

    Revert "fix: change conditional ECR import to import block"
    This reverts commit 7cd7af187c1326b7f9891eed2ddb75114922d37a.

--- deployment/ecr/ecr.tf
+++ deployment/ecr/ecr.tf
@@ -3,11 +3,6 @@ resource "aws_ecr_repository" "my-ecr-repository" {
   name = "dummy-app-ecr-repository"
 }
 
-import {
-  to = aws_ecr_repository.my-ecr-repository
-  id = "dummy-app-ecr-repository"
-}
-
 # Output the ECR repository URL to consume in the GitHub Actions pipeline
 output "dummy-app-ecr-repository-url" {
   value = aws_ecr_repository.my-ecr-repository.repository_url
