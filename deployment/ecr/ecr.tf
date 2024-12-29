177782578a50da97254795f389e753e87b822985
Josu Martinez <josu.martinez@gmail.com>
2024-12-29 18:30:46 +0100

    feat: retain most recent image

--- deployment/ecr/ecr.tf
+++ deployment/ecr/ecr.tf
@@ -3,6 +3,28 @@ resource "aws_ecr_repository" "my-ecr-repository" {
   name = "dummy-app-ecr-repository"
 }
 
+# ECR policy to retain only the most recent image
+resource "aws_ecr_lifecycle_policy" "my_ecr_policy" {
+  repository = aws_ecr_repository.my-ecr-repository.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Retain only the most recent image"
+        selection = {
+          tagStatus    = "any"
+          countType    = "imageCountMoreThan"
+          countNumber  = 1
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
 # Output the ECR repository URL to consume in the GitHub Actions pipeline
 output "dummy-app-ecr-repository-url" {
   value = aws_ecr_repository.my-ecr-repository.repository_url
