8158a44218a048c28b2648228ca8e09378adc07c
Josu Martinez <josu.martinez@gmail.com>
2025-01-11 18:40:08 +0100

    feat: enable non-empty Terraform state bucket destruction

--- deployment/s3/s3.tf
+++ deployment/s3/s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "dummy-app-terraform-state-bucket" {
   bucket = "dummy-app-terraform-state-bucket"
+  force_destroy = true # Ensures all objects are deleted before the bucket is destroyed
   
   tags = {
     Name = "dummy-app-terraform-state-bucket"
