01ee646220f7794172fa46142fa6c5024eda420a
Josu Martinez <josu.martinez@gmail.com>
2025-08-10 00:08:45 +0200

    fix: update hosted zone with trailing dot

--- deployment/ecs/route53.tf
+++ deployment/ecs/route53.tf
@@ -1,6 +1,6 @@
 # Route 53 hosted zone, a container of DNS records of a domain. It manages where the domain points to (e.g., a web server).
 resource "aws_route53_zone" "my-domain-zone" {
-  name    = "josumartinez.com"
+  name    = "josumartinez.com."
   comment = "Hosted zone for my domain"
 }
 
