b4ee5802271779deeb7347a888f8db0c1b5b4671
Josu Martinez <josu.martinez@gmail.com>
2025-08-10 15:20:22 +0200

    feat: add comment in code
    So that CloudWatch registers it as an app log.

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log("Return 'hello world' from the app service");
     return 'Hello World!';
   }
 }
