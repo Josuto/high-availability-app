2e9561f45008a3279891253622c8d9ebd98fc480
Josu Martinez <josu.martinez@gmail.com>
2025-08-20 18:58:39 +0200

    feat: add console log to display app instance ID

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,7 @@ export class AppService {
   constructor(@Inject(APP_INSTANCE_ID_TOKEN) private readonly appInstanceId: string) {}
 
   getHello(): string {
-    console.log(`Return 'hello world' from the service instance ${this.appInstanceId}`);
+    console.log(`Return 'hello world' from the service of the app instance with ID ${this.appInstanceId}`);
     return 'Hello World!';
   }
 }
