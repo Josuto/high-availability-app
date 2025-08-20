f7c8566826123281382a036997ef5c00a3abe4e3
Josu Martinez <josu.martinez@gmail.com>
2025-08-20 18:27:28 +0200

    feat: add random number to distinguish app instance

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { randomUUID } from 'crypto';
+import { APP_INSTANCE_ID_TOKEN } from './constants';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INSTANCE_ID_TOKEN,
+      useValue: randomUUID(), // Generate the ID using crypto
+    },
+  ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { APP_INSTANCE_ID_TOKEN } from './constants';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject(APP_INSTANCE_ID_TOKEN) private readonly appInstanceId: string) {}
+
   getHello(): string {
-    console.log("Return 'hello world' from the app service");
+    console.log(`Return 'hello world' from the service instance ${this.appInstanceId}`);
     return 'Hello World!';
   }
 }
