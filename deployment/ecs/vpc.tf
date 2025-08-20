5853708fc95673cc311fa27290d1795eee337ae7
Josu Martinez <josu.martinez@gmail.com>
2025-08-20 18:54:31 +0200

    docs: add comment on enable_nat_gateway property

--- deployment/ecs/vpc.tf
+++ deployment/ecs/vpc.tf
@@ -8,7 +8,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway = true # Required to be true as otherwise the EC2 instance is not able to register in the ECS cluster or access any other AWS external service e.g., SSM
   single_nat_gateway = true # Creates one NAT GW for all AZs (cheaper option in test envs not requiring high-availability)
   enable_vpn_gateway = false
 
