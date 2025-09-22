8455fb49bd1f715d7bc139f34ad598106ff8976c
Josu Martinez <josu.martinez@gmail.com>
2025-09-22 23:03:26 +0200

    ci: enable ECR repo name spec at root module config

--- infra/deployment/ecr/outputs.tf
+++ infra/deployment/ecr/outputs.tf
@@ -2,3 +2,7 @@
 output "ecr_repository_url" {
   value = module.ecr.ecr_repository_url
 }
+
+output "ecr_repository_name" {
+  value = module.ecr.ecr_repository_name
+}
--- infra/modules/ecr/outputs.tf
+++ infra/modules/ecr/outputs.tf
@@ -2,3 +2,7 @@
 output "ecr_repository_url" {
   value = aws_ecr_repository.app-ecr-repository.repository_url
 }
+
+output "ecr_repository_name" {
+  value = aws_ecr_repository.app-ecr-repository.name
+}
