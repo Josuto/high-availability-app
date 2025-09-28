bfccb4815384bfe7a221af7457a1d192f1e5c17b
Josu Martinez <josu.martinez@gmail.com>
2025-09-28 23:15:59 +0200

    fix: add missing module output

--- infra/modules/ecs_cluster/outputs.tf
+++ infra/modules/ecs_cluster/outputs.tf
@@ -1,13 +1,13 @@
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.ecs-cluster.name
+}
+
 output "ecs_cluster_arn" {
   value = aws_ecs_cluster.ecs-cluster.arn
 }
 
 output "ecs_security_group_id" {
   value = aws_security_group.cluster.id
-}
-
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.ecs-cluster.name
 }
 
 output "autoscaling_group_name" {
