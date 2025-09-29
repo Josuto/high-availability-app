b9ec1863c5507603760cc7dc7d8df99cf21f9982
Josu Martinez <josu.martinez@gmail.com>
2025-09-29 12:28:25 +0200

    feature: increase ECS max utilisation percentage

--- infra/deployment/prod/ecs_cluster/vars.tf
+++ infra/deployment/prod/ecs_cluster/vars.tf
@@ -3,15 +3,15 @@ variable "project_name" {
   type        = string
 }
 
-variable "cpu_scale_up_alarm_thresold" {
-  description = "The percentage of CPU utilisation that makes the scale up alarm go off"
-  default     = 70
-}
+# variable "cpu_scale_up_alarm_thresold" {
+#   description = "The percentage of CPU utilisation that makes the scale up alarm go off"
+#   default     = 70
+# }
 
-variable "cpu_scale_down_alarm_thresold" {
-  description = "The percentage of CPU utilisation that makes the scale down alarm go off"
-  default     = 25
-}
+# variable "cpu_scale_down_alarm_thresold" {
+#   description = "The percentage of CPU utilisation that makes the scale down alarm go off"
+#   default     = 25
+# }
 
 variable "cpu_alarm_notification_email" {
   description = "The email to notify to when a CPU utilisation alarm goes off"
@@ -20,5 +20,5 @@ variable "cpu_alarm_notification_email" {
 
 variable "ecs_max_utilisation" {
   description = "Max utilisation of the ECS cluster percentage-wise. The ECS calculates its current utilisation by masuring the total CPU/memory capacity reserved by the running tasks against the total available capacity of all EC2 instances of the cluster"
-  default     = 10
+  default     = 75
 }
