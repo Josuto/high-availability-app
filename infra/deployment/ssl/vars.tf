de584dcbad12b4ae550bcaa57302d8518d40512c
Josu Martinez <josu.martinez@gmail.com>
2025-11-22 20:14:07 +0100

    fix: add mising environment variable in root module

--- infra/deployment/ssl/vars.tf
+++ infra/deployment/ssl/vars.tf
@@ -12,3 +12,13 @@ variable "root_domain" {
   description = "The root domain name for SSL certificate (e.g., example.com)"
   type        = string
 }
+
+variable "environment" {
+  description = "The environment to deploy to (dev or prod)."
+  type        = string
+  validation {
+    condition     = contains(["dev", "prod"], var.environment)
+    error_message = "The environment must be either 'dev' or 'prod'."
+  }
+}
+
