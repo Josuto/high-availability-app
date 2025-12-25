4d5cd2dbb0db0db1d48a28152d35787da92b2f2a
Josu Martinez <josu.martinez@gmail.com>
2025-12-25 23:09:41 +0100

    debug: add further debug resources

--- infra-eks/modules/k8s_app_deployment/main.tf
+++ infra-eks/modules/k8s_app_deployment/main.tf
@@ -133,8 +133,8 @@ resource "kubernetes_deployment" "app" {
   wait_for_rollout = true
 
   timeouts {
-    create = "10m"
-    update = "10m"
+    create = "15m"
+    update = "15m"
     delete = "5m"
   }
 }
