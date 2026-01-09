81713e52a6d88788624800b6e4f725052e503b2a
Josu Martinez <josu.martinez@gmail.com>
2026-01-09 12:29:15 +0100

    fix: restore output variable
    The intent of this commit is to perform some debugging in a pre-commit hook introduced to kick in TF validation.

--- infra-ecs/deployment/ssl/outputs.tf
+++ infra-ecs/deployment/ssl/outputs.tf
@@ -1,4 +1,4 @@
-# output "acm_certificate_validation_arn" {
-#   description = "The ARN of the validated ACM certificate for HTTPS connections"
-#   value       = module.ssl.acm_certificate_validation_arn
-# }
+output "acm_certificate_validation_arn" {
+  description = "The ARN of the validated ACM certificate for HTTPS connections"
+  value       = module.ssl.acm_certificate_validation_arn
+}
